cd7be2358b59ae72a32eab5be6f75a3f11f5fa6c
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-04-04 16:10:33 +0600

    Updated project settings

--- src/prepareData.test.js
+++ src/prepareData.test.js
@@ -1,16 +1,16 @@
+const { findCurrentSprint } = require('./prepareData');
 const input = require('../examples/input.json');
 
-const { findCurrentSprint } = require('./prepareData');
+const CURRENT_SPRINT = {
+  id: 977,
+  type: 'Sprint',
+  name: 'Последний вагон',
+  startAt: 1603573502000,
+  finishAt: 1604178302000,
+};
 
 test('Find current sprint: existing', () => {
-  const expected = {
-    id: 977,
-    type: 'Sprint',
-    name: 'Последний вагон',
-    startAt: 1603573502000,
-    finishAt: 1604178302000,
-  };
-  expect(findCurrentSprint(input, 977)).toStrictEqual(expected);
+  expect(findCurrentSprint(input, 977)).toStrictEqual(CURRENT_SPRINT);
 });
 
 test('Find current sprint: non-existing', () => {
